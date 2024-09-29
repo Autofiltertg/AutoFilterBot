7645a508657e21b73d180f8e409ea18419ca671a
Autofiltertg <t.metghj123@gmail.com>
2024-09-30 01:57:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,21 +1,16 @@
 class script(object):
-    START_TXT = """<b><i>𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍</b></i>"""
-    HELP_TXT = """<b>𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂.</b>"""
+    START_TXT = """<b>ᯓᡣ𐭩 ʜᴇʏ {},
+‎‧₊˚✧ [ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇ's ᴀɴᴅ sᴇʀɪᴇ's,
+ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴇɴᴊᴏʏ.
+ɪ ᴡᴏʀᴋ ᴏɴ ʙᴏᴛʜ ɢʀᴏᴜᴘ ᴀɴᴅ ᴘᴍ.‎‎] ✧˚₊‧</b>"""
+    HELP_TXT = """<b>Hᴇʏ {},
+    ─── ⋆⋅☆⋅⋆ ──
+Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
     ABOUT_TXT = """<b>✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Mrz_bots>Creator</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]</b>"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Tᴏɴʏ Sᴛᴀʀᴋ Is Oᴘᴇɴ Pʀᴏᴊᴇᴄᴛ. 
-- Sᴏᴜʀᴄᴇ  - https://github.com/Mrzbots/AutoFilterBot  
-
-<b>DEVS:</b>
-- <a href=https://t.me/Mrz_bots>Creator</a>"""
+✯ Cʀᴇᴀᴛᴏʀ : <a href=https://t.me/chillxnow>JAi Shree Ram 🚩</a>
+✯ Uᴘᴅᴀᴛᴇs : <a href=https://t.me/Mg_networks>MG Nᴇᴛᴡᴏʀᴋ ✮⋆˙</a>
+✯ Bᴜɪʟᴅ Sᴛᴀᴛᴜs : v1.0.1 [ 𝙱𝙴𝚃𝙰 ]</b>"""
+    SOURCE_TXT = """<b>Cᴏɴᴛᴀᴄᴛ Tᴏ Dᴇᴠʟᴏᴘᴇʀ : <a href=https://t.me/chillxnow>JAɪ Sʜʀᴇᴇ Rᴀᴍ 🚩</a></b>
     TELEGRAPH = """Reply to any photo or video using by /telegraph that photo telegraph link you will get"""
     TOOLS = """TOOLS:
 
